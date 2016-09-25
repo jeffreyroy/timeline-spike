37d071c2bb0b28e212bd35a30f9c22f1e3787bb8
Jeff Roy <jeffrey_a_roy@yahoo.com>
2016-09-24 17:25:48 -0700

    Add bar chart

--- app/assets/javascripts/d3-test.js
+++ app/assets/javascripts/d3-test.js
@@ -11,9 +11,34 @@ var clickListener = function() {
 }
 
 var doStuff = function() {
-  var dataset = [ 5, 10, 15, 20, 25 ];
-  container = d3.select("#container").selectAll("p");
-  console.log(container);
-  container.data(dataset).enter().append("p").text("Hi!");
+  var barData = [ 25, 7, 5, 26, 11, 8, 25, 14, 23, 19,
+                14, 11, 22, 29, 11, 13, 12, 17, 18, 10,
+                24, 18, 25, 9, 3 ];
+  barChart(barData);
+}
 
+var barChart = function(dataset) {
+  container = d3.select("#container").selectAll("p").data(dataset);
+  console.log(container);
+  container.enter()
+    .append("div")
+    .attr("class", "bar")
+    .style("height", function(d) {
+      var barHeight = d * 5;
+      return barHeight + "px";
+    })
+    .style("background-color", function(d) {
+      var g = d * 8;
+      return "rgb(" + g + "," + g + "," + g + ")";
+    })
+    ;
+}
+
+var circles = function(dataset) {
+
+}
+
+
+var calculateStuff = function(n) {
+  return n;
 }
